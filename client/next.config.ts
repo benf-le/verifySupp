6462ec2b9b205ccbcc88b8072dc48bac9da0b5b2
Cẩm Bằng Lê <banglebang2016@gmail.com>
2025-08-11 16:45:13 +0700

    create home page

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+        domains: [
+            'cdn.nhathuoclongchau.com.vn',
+            'cms-prod.s3-sgn09.fptcloud.com',
+        ],
+    },
+
 };
 
 export default nextConfig;
