7e15624b6f8ab67bae350d3dcd4336dad1bb59fb
Cẩm Bằng Lê <banglebang2016@gmail.com>
2025-08-11 13:50:36 +0700

    create navbar

--- client/components/navbar.tsx
+++ client/components/navbar.tsx
@@ -1,11 +1,19 @@
 import Link from "next/link";
 
 export const Navbar = () => {
-    return (<nav>
-        <div>
-            <Link href='/'>
+    return (<nav className='sticky top-0 z-50 bg-green-600 shadow'>
+        <div className='container mx-auto flex items-center justify-between p-12'>
+            <Link href='/' className='hover:text-white font-bold'>
                 Logo
             </Link>
+             <div className='hidden md:flex space-x-6'>
+                 <Link href={'/checkout'} className='hover:text-white font-bold'>
+                     Checkout</Link>
+
+             </div>
+
+
         </div>
+
     </nav>)
 }
