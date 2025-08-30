cf489bf452141a21851f3044e25438750bb6f89c
benf-le <banglebang2016@gmail.com>
2025-08-30 15:32:33 +0700

    change path API

--- client/src/services/UserService.ts
+++ client/src/services/UserService.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
+import {BASE_URL} from "../constant/appInfo.ts";
 
 export const axiosJWT = axios.create()
 
 export const loginUser = async (data) => {
-    const res = await axios.post(`http://localhost:7000/login`, data)
+    const res = await axios.post(BASE_URL+`/login`, data)
     return res.data
 }
 
 export const getDetailsUser = async (id, access_token) => {
-    const res = await axiosJWT.get(`http://localhost:7000/user/me/${id}`, {
+    const res = await axiosJWT.get(BASE_URL+`/user/me/${id}`, {
         headers: {
             Authorization: `Bearer ${access_token}`,
         },
