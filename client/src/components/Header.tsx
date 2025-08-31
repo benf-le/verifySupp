da94785492318dcd4e9abf995ac2219fc628cf3e
benf-le <banglebang2016@gmail.com>
2025-09-01 01:29:26 +0700

    edit css header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from 'react'
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import HandleProducts from "../api/HandleProducts";
 
 import {AiOutlineUser} from "react-icons/ai";
@@ -13,52 +13,36 @@ import {useSelector} from "react-redux";
 import {RootState} from "../redux/store.ts";
 
 export default function Header() {
-    const [collections, setCollections] = useState<Collection[]>([]) //Products co dang array
-    // const [open, setOpen] = useState(false)
-
+    const [collections, setCollections] = useState<Collection[]>([])
     const [user, setUser] = useState('')
-    // @ts-ignore
     const [type, setType] = useState('')
-
-    // @ts-ignore
-    const [cookies, setCookie, removeCookie] = useCookies(['AuthToken'])
-
+    const [cookies, , removeCookie] = useCookies(['AuthToken'])
     const authToken = cookies.AuthToken
-    console.log(authToken)
 
     const cartItems = useSelector((state: RootState) => state.cart.items);
     const cartCount = cartItems.reduce((sum, item) => sum + item.qty, 0);
 
-    // const navigate = useNavigate()
-    const getUser = async () => {
-        try {
-
-            const decode_token_user = jwt_decode(authToken)
-
-            // console.log(decode_token_user)
-            // @ts-ignore
-            const ten = decode_token_user.firstName
-            // @ts-ignore
-            const user__type = decode_token_user.userType
-            // console.log(user__type)
-            setUser(ten)
-            setType(user__type)
-
-
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
+    const location = useLocation(); // lấy đường dẫn hiện tại để check active
 
     useEffect(() => {
         getUser()
         getCollections()
     }, [])
 
+    const getUser = async () => {
+        try {
+            const decode_token_user = jwt_decode(authToken)
+            // @ts-ignore
+            setUser(decode_token_user.firstName || "")
+            // @ts-ignore
+            setType(decode_token_user.userType || "")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     const getCollections = async () => {
         const api = `/collections`
-
         try {
             const res: any = await HandleProducts.getProducts(api)
             if (res) {
@@ -69,175 +53,97 @@ export default function Header() {
         }
     }
 
-    //lấy ra phần tử cuối cùng trong mảng
     const lastCollection = collections.length > 0 ? collections[collections.length - 1] : null;
 
     function Logout() {
-
         removeCookie('AuthToken', cookies.AuthToken)
-        window.location.reload() //nen sua cho nay de load lai trang chu
+        window.location.reload()
     }
 
-
     return (
-
         <div>
+            {/* HEADER */}
+            <header className="h-20">
+                <div className="verify-supp-color navbar flex h-20 flex-grow fixed left-0 right-0 top-0 z-10">
+                    <div className="basis-1/4 justify-center">
+                        <Link to="/" className="btn btn-ghost text-xl text-white">VerifySupp</Link>
+                    </div>
 
-            <div>
+                    <div className="basis-1/2 ">
+                        <ButtonInputSearch placeholder="Search"/>
+                    </div>
 
-                <header className="h-20">
-                    <div
-                        className=" verify-supp-color navbar  flex h-20 flex-grow bg-base-100 fixed left-0 right-0 top-0 z-10">
-
-                        <div className="basis-1/4 justify-center">
-                            <Link to="/" className="btn btn-ghost text-xl text-white">VerifySupp</Link>
-                        </div>
-
-
-                        <div className="basis-1/2 ">
-                            <ButtonInputSearch
-                                placeholder="Search"
-                            />
-
-                        </div>
-                        <div className="basis-1/4 justify-center">
-                            {!authToken ?
-                                <Link to="/login">
-                                    <button className="btn btn-ghost text-white">
-                                        <AiOutlineUser/>
-                                        Login
-                                    </button>
-                                </Link> :
-
-                                <div className="dropdown dropdown-end">
-                                    <label tabIndex={0} className="btn btn-ghost text-white">
-                                        {/*<div className="w-10 rounded-full">*/}
-                                        {/*    <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg"/>*/}
-                                        {/*</div>*/}
-                                        {user}
-                                    </label>
-                                    <ul tabIndex={0}
-                                        className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                                        <li>
-                                            <Link to='/user/profile'>
-                                                <a className="justify-between">
-                                                    Profile
-                                                </a></Link>
-                                        </li>
-                                        {/*<li><a>Settings</a></li>*/}
-                                        <li onClick={Logout}><a>Logout</a></li>
-                                    </ul>
-                                </div>
-                            }
-                            <Link to="/cart">
-                                <button className="btn btn-ghost text-white relative">
-                                    <BsCart3/>
-                                    {cartCount > 0 && (
-                                        <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs rounded-full px-2">
-                                {cartCount}
-                            </span>
-                                    )}
+                    <div className="basis-1/4 justify-center">
+                        {!authToken ? (
+                            <Link to="/login">
+                                <button className="btn btn-ghost text-white">
+                                    <AiOutlineUser/> Login
                                 </button>
                             </Link>
-                        </div>
-                    </div>
-                </header>
-                {/*<menu className="verify-supp-color navbar flex h-20 flex-grow justify-center bg-base-100">*/}
-                {/*  <div className="join space-x-10">*/}
-                {/*    <button className="btn btn-ghost text-white">Dog</button>*/}
-                {/*    <button className="btn btn-ghost text-white">Cat</button>*/}
-                {/*    <button className="btn btn-ghost text-white">Fish</button>*/}
-                {/*    <button className="btn btn-ghost text-white">Horse</button>*/}
-                {/*    <button className="btn btn-ghost text-white">Bird</button>*/}
-                {/*    <button className="btn btn-ghost text-white">Small Animal</button>*/}
-                {/*    <button className="btn btn-ghost text-white">Reptile</button>*/}
-                {/*    <button className="btn btn-ghost text-white">Pet Service</button>*/}
-                {/*    <button className="btn btn-ghost text-white">More</button>*/}
-                {/*    <button className="btn btn-ghost bg-red-600 text-white">Sale</button>*/}
-                {/*  </div>*/}
-                {/*</menu>*/}
-
-                <menu>
-                    <div className=" verify-supp-color navbar bg-base-100  ">
-                        <div className="">
-                            <div className="dropdown">
-                                <label tabIndex={0} className="btn  lg:hidden">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="h-5 w-5"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            d="M4 6h16M4 12h8m-8 6h16"
-                                        />
-                                    </svg>
+                        ) : (
+                            <div className="dropdown dropdown-end">
+                                <label tabIndex={0} className="btn btn-ghost text-white">
+                                    {user}
                                 </label>
-                                <ul
-                                    tabIndex={0}
-                                    className="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow"
-                                >
+                                <ul tabIndex={0}
+                                    className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
                                     <li>
-                                        <a>Item 1</a>
+                                        <Link to='/user/profile'>Profile</Link>
                                     </li>
-                                    <li>
-                                        <a>Parent</a>
-                                        <ul className="p-2">
-                                            <li>
-                                                <a>Submenu 1</a>
-                                            </li>
-                                            <li>
-                                                <a>Submenu 2</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a>Item 3</a>
-                                    </li>
+                                    <li onClick={Logout}><a>Logout</a></li>
                                 </ul>
                             </div>
-                        </div>
-
-
-                        <div className="navbar flex hidden h-8 flex-grow justify-center lg:flex m-0">
-                            <ul className="verify-supp-color menu menu-horizontal space-x-5 px-1">
-                                {collections.length > 0 && collections.slice(0, collections.length - 1).map(collec =>
-                                    <Link to={`/collections/${collec.name}`}>
-                                        <div className="dropdown dropdown-hover">
-                                            <label tabIndex={0}
-                                                   className="btn m-1 text-white verify-supp-color border-none hover:text-sky-900 text-lg">
-                                                {collec.name}
-                                            </label>
-                                            {/*<ul tabIndex={0}*/}
-                                            {/*    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">*/}
-                                            {/*    <li><a>Item 1</a></li>*/}
-                                            {/*    <li><a>Item 2</a></li>*/}
-                                            {/*</ul>*/}
-                                        </div>
-                                    </Link>
+                        )}
+                        <Link to="/cart">
+                            <button className="btn btn-ghost text-white relative">
+                                <BsCart3/>
+                                {cartCount > 0 && (
+                                    <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs rounded-full px-2">
+                                      {cartCount}
+                                    </span>
                                 )}
-
-
-                                <button className="btn btn-ghost bg-red-600 text-white">
-
-                                    <Link to={`/collections/${lastCollection?.name}`}>
-                                        {lastCollection?.name}
-                                    </Link>
-                                </button>
-                            </ul>
-                        </div>
+                            </button>
+                        </Link>
                     </div>
-                </menu>
+                </div>
+            </header>
 
+            {/* MENU COLLECTIONS */}
+            <menu >
+                <div className="navbar bg-white border-b">
+                    <div className="hidden lg:flex mx-auto">
+                        <ul className="flex space-x-5 px-1">
+                            {collections.length > 0 && collections.slice(0, collections.length - 1).map(collec => {
+                                const active = location.pathname.includes(`/collections/${collec.name}`)
+                                return (
+                                    <Link to={`/collections/${collec.name}`} key={collec.name}>
+                                        <div
+                                            className={`px-4 py-2 text-lg cursor-pointer 
+                                            ${active ? "text-sky-900 border-b-4" : "text-gray-700 hover:text-sky-900"}`}
+                                            style={active ? { borderBottomColor: "rgb(13, 145, 5)" } : {}}
+                                        >
+                                            {collec.name}
+                                        </div>
 
-            </div>
+                                    </Link>
+                                )
+                            })}
 
-
+                            {lastCollection && (
+                                <Link to={`/collections/${lastCollection.name}`}>
+                                    <div
+                                        className={`px-4 py-2 text-lg cursor-pointer 
+                                            ${location.pathname.includes(`/collections/${lastCollection.name}`)
+                                            ? "text-sky-900 border-b-4 border-verify-supp-color"
+                                            : "bg-red-600 text-white rounded-md"}`}>
+                                        {lastCollection.name}
+                                    </div>
+                                </Link>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            </menu>
         </div>
     );
-
 }
