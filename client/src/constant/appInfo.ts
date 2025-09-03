0e8f82a97dd23aab66f76a0cec03470bf05098b3
benf-le <banglebang2016@gmail.com>
2025-09-03 20:42:10 +0700

    fix CHATWOOT_URL from http to https to run on gcp

--- client/src/constant/appInfo.ts
+++ client/src/constant/appInfo.ts
@@ -1,4 +1,5 @@
 // export const BASE_URL =`http://localhost:7000`
 // export const BASE_URL =`https://verifysupp-nest-app-service-242459576772.us-central1.run.app`
 export const BASE_URL =`https://be-verifysupp.lecambang.id.vn`
-export const CHATWOOT_URL=`http://34.55.138.114:3000`
+// export const CHATWOOT_URL=`http://34.55.138.114:3000` run local
+export const CHATWOOT_URL=`https://34.55.138.114:3000` //run gcp
