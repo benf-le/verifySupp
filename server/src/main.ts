0ca418637bf97be08d28ab51960278033120b617
benf-le <banglebang2016@gmail.com>
2025-09-03 10:23:37 +0700

    enableCors from domain frontend

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,11 @@ import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://verifysupp.lecambang.id.vn', // Thay bằng domain frontend của bạn
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   // add middleware HERE!
   app.useGlobalPipes(new ValidationPipe()); //request gửi đến controller thì đi qua ValidationPipe
