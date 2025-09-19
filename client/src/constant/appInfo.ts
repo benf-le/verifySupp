08f90b57c92bdaf51042a730b3446e7548190d2a
benf-le <banglebang2016@gmail.com>
2025-09-19 15:25:31 +0700

    update CHATWOOT_URL

--- client/src/constant/appInfo.ts
+++ client/src/constant/appInfo.ts
@@ -2,4 +2,4 @@
 // export const BASE_URL =`https://verifysupp-nest-app-service-242459576772.us-central1.run.app`
 export const BASE_URL =`https://be-verifysupp.lecambang.id.vn`
 // export const CHATWOOT_URL=`http://34.55.138.114:3000` run local
-export const CHATWOOT_URL=`https://chatwoot.lecambang.id.vn` //run gcp
+export const CHATWOOT_URL=`https://be-verifysupp.lecambang.id.vn/chatwoot` //run gcp
