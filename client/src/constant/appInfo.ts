d26423204b3bc208236e59bb950556c908a7061a
benf-le <banglebang2016@gmail.com>
2026-01-06 02:48:54 +0700

    change api

--- client/src/constant/appInfo.ts
+++ client/src/constant/appInfo.ts
@@ -1,6 +1,5 @@
+// export const BASE_URL =`http://localhost:7000`
 export const BASE_URL =`https://be-verifysupp.lecambang.id.vn`
-// export const BASE_URL =`https://be-verifysupp.lecambang.id.vn`
 // export const CHATWOOT_URL=`http://34.55.138.114:3000` run local
 export const CHATWOOT_WEBSITE_TOKEN='ujffpSvAmgGaaBBhs1qA45a1'
 export const CHATWOOT_URL=`https://chatwoot.lecambang.id.vn` //run gcp
-    
